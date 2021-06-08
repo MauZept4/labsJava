a2fb6aeba93f8879584802a1b03d3ac03cf2cd90
MauZept4 <mauzept4@gmail.com>
2021-06-08 17:10:46 +0300

    nice dihanie

--- src/main/java/com/example/tourism/repository/LuggageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.tourism.repository;
-
-import com.example.tourism.model.Luggage;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LuggageRepository extends MongoRepository<Luggage, String > {
-}
--- src/main/java/com/example/tourism/repository/TouristRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.tourism.repository;
-
-import com.example.tourism.model.Tourist;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface  TouristRepository extends MongoRepository<Tourist, String > {
-}
